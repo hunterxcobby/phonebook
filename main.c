7c6f0879d969e8824653c96faacff345d449d18b
hunterxcobby <solomonsefah13@gmail.com>
2023-11-27 20:39:57 +0000

    more cool features

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 int main(void)
 {
     printf("Hello World\n");
-    printf("Welcome to CLI phonebook\n");
+    printf("Display menu\n");
+    printf("Display usage of commands\n");
+    printf("Need help with usage ?\n");
 
     return(0);
 
